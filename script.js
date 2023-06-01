6720d6ef9afbd63159d7dc8d836da8dc91e0e696
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-01 11:12:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+const user = {
+    name: "Dima",
+    4: 30,
+    birthDay: "dd/mm/yyyy",
+    showMyPublicData: function(){
+        console.log(`${this.name} ${this.birthDay}`);
+    }
+}
+
+//console.log(user);
+//console.log(Object.keys(user));
+//console.log(Object.values(user));
+
+const shops = [
+    {oil: 200},
+    {rice: 500},
+    {bread: 60},
+];
+
+const budget = [5000, 15000, 30000];
+
+const map = new Map();
+//console.log(map);
+
+/*
+
+map.set(shops[0], 5000);
+map.set(shops[1], 4000);
+map.set(shops[2], 15000);
+console.log(map);
+
+*/
+
+shops.forEach((shop, i) => {
+    map.set(shop, budget[i]);
+})
+
+//console.log(map);
+//console.log(map.get(shops[0]));
+//console.log(map.has(shops[0]));
+//console.log(map.has(shops[4]));
+//map.delete(shops[0]);
+//console.log(map);
+//map.clear();
+//console.log(map);
+
